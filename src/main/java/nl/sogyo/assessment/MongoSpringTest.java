35e7d58e4168b37070c171f7656d4f9e2141fbeb
Gilles Wijnker <gwijnker@sogyo.nl>
2018-04-06 15:28:47 +0200

    commit before applying some naming/folder changes

--- src/main/java/nl/sogyo/assessment/MongoSpringTest.java
+++ src/main/java/nl/sogyo/assessment/MongoSpringTest.java
@@ -2,47 +2,45 @@ package nl.sogyo.assessment;
 
 import nl.sogyo.assessment.domain.DataNavigator;
 import nl.sogyo.assessment.domain.DatabaseEntity;
+import nl.sogyo.assessment.domain.IDataNavigator;
 import nl.sogyo.assessment.repositories.DatabaseRepository;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Page;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
-@SpringBootApplication
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
+
+//@SpringBootApplication
 public class MongoSpringTest implements CommandLineRunner{
 	public static void main(String[] args) {
+		System.out.println("\n\n\n===================\n\n\nMAIN\n\n\n===================");
 		SpringApplication.run(MongoSpringTest.class, args);
 	}
-	
-//	@Autowired
-//	BeanFactory beanFactory;
-	
-//	@Autowired
-//	DatabaseRepository databaseRepository;
 	
 	@Autowired
 	DataNavigator dataNavigator;
 	
 	@Override
 	public void run(String... args) {
-//		DataNavigator dn = beanFactory.getBean(DataNavigator.class, "itter");
-//		DataNavigator dn = new DataNavigator("itter");
-		dataNavigator.executeQuery("itter", 0, 10);
-		System.out.println(dataNavigator.getTotalElements());
-//		//
-//		int pageSize = 2;
-//		int page = 0;
+		IDataNavigator dn = dataNavigator.executeQuery("itter", 0, 10);
+		System.out.println(dn.getTotalElements());
+		System.out.println(dn.toJson());
+//		Integer[] l = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+//		Pageable pageable = PageRequest.of(0, 5);
+//		Page<Integer> page = new PageImpl<Integer>(Arrays.asList(l), pageable, 16);
 //		
-//		Pageable pageable = PageRequest.of(page, pageSize); 
-//		Page<DatabaseEntity> pageDB = databaseRepository.findAll(pageable);
-//		long nrItems = pageDB.getTotalElements();
-//		System.out.println(nrItems);
-//		//
-//		System.out.println(dataNavigator.getTotalElements());
+//		System.out.println(page.getNumberOfElements());
+//		System.out.println(page.getNumber());
+//		System.out.println(page.getSize());
+//		System.out.println(page.getContent());
 	}
 }
